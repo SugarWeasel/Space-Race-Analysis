7c197bc73ab43b9fd2cc91794a344f44a3f7dc32
Kenneth <kennethn911@gmail.com>
2024-10-09 19:12:03 -0400

    Plotted US vs USSR during cold war, finish remaining analysis.

--- Space_Missions_Analysis.py
+++ Space_Missions_Analysis.py
@@ -13,7 +13,7 @@
 
 # ### Import Statements
 
-# In[489]:
+# In[216]:
 
 
 import numpy as np
@@ -28,9 +28,11 @@ from datetime import datetime, timedelta
 from dateutil import parser
 
 
+
+
 # ### Notebook Presentation
 
-# In[490]:
+# In[217]:
 
 
 pd.options.display.float_format = '{:,.2f}'.format
@@ -38,7 +40,7 @@ pd.options.display.float_format = '{:,.2f}'.format
 
 # ### Load the Data
 
-# In[491]:
+# In[218]:
 
 
 df_data = pd.read_csv("mission_launches.csv")
@@ -51,15 +53,13 @@ df_data = pd.read_csv("mission_launches.csv")
 # * What are the column names?
 # * Are there any NaN values or duplicates?
 
-# In[492]:
+# In[219]:
 
 
-df_data.shape
+print(f"The dataframe has {df_data.shape[0]} rows and {df_data.shape[1]} columns")
 
 
-# <p>The dataframe has 9 columns, with 4324 rows of data.</p>
-
-# In[493]:
+# In[220]:
 
 
 df_data.info()
@@ -69,35 +69,33 @@ df_data.info()
 
 # ## Checking for duplicates and NaN values ##
 
-# In[494]:
+# In[221]:
 
 
 #checking for NaN values
-df_data.isna().values.any()
+print(f"When checking for NAN the result returns {df_data.isna().values.any()}")
 
 
-# In[495]:
+# In[222]:
 
 
 print(f"There are {df_data.isna().values.sum()} missing values in the dataframe.")
 
 
-# In[496]:
+# In[223]:
 
 
 df_data.isna().sum()
 
 
-# In[497]:
+# In[224]:
 
 
 #checking for duplicate values
-df_data.duplicated().values.any()
+print(f"When checking for NAN the result returns {df_data.duplicated().values.any()}")
 
 
-# <p>The dataset contains no duplicate entries, but does contain Nan values</p>
-
-# In[498]:
+# In[225]:
 
 
 #dropping the Nan values and using a separate Dataframe in case cost comparisons need to be made.
@@ -105,7 +103,7 @@ df_data_clean = df_data.dropna()
 df_data_clean.isna().values.any()
 
 
-# In[499]:
+# In[226]:
 
 
 df_data_clean.info()
@@ -115,14 +113,13 @@ df_data_clean.info()
 # 
 # Creating a chart that shows the number of space mission launches by organisation.
 
-# In[500]:
+# In[227]:
 
 
 launches_by_org = df_data.Organisation.value_counts()
-launches_by_org
 
 
-# In[501]:
+# In[228]:
 
 
 launches = px.bar(launches_by_org, 
@@ -143,14 +140,13 @@ launches.show()
 # 
 # How many rockets are active compared to those that are decomissioned? 
 
-# In[502]:
+# In[229]:
 
 
 status = df_data.Rocket_Status.value_counts()
-status
 
 
-# In[503]:
+# In[230]:
 
 
 # We will do a bar char to visualize this, but first let's provide a list of values for the x axis (x = ["Yes", "No"]) instead of using the index
@@ -172,7 +168,7 @@ rocket_status.show()
 # How many missions were successful?
 # How many missions failed?
 
-# In[504]:
+# In[231]:
 
 
 df_data.Mission_Status.value_counts()
@@ -184,7 +180,7 @@ df_data.Mission_Status.value_counts()
 # 
 # Create a histogram and visualise the distribution. The price column is given in USD millions (careful of missing values). 
 
-# In[505]:
+# In[232]:
 
 
 # the histogram will be sorted by price in ascending order
@@ -217,17 +213,11 @@ price_hist.update_layout(xaxis_title = "Cost of Mission in $ Millions")
 # 
 # You can use the iso3166 package to convert the country names to Alpha3 format.
 
-# In[506]:
-
-
-df_data.Location.value_counts()
-
-
 # <p>Russia, Barents Sea, Sharud, Pacific Missile Range, North Korea, Gran Canaria need to be converted. </p>
 
 # <h3>Changing some locations to country names.</h3>
 
-# In[507]:
+# In[233]:
 
 
 # Use manual mapping
@@ -245,16 +235,18 @@ location_dict = {
 
 # <h3>Converting string to ISO</h3>
 
-# In[508]:
+# In[234]:
 
 
-# Can be done by splitting the each location by comma and getting that data. For example:
+# Can be done by splitting the each location by comma and getting that data. 
+#For example let's use JAPAN
+print(df_data.Location[200])
 country_name = df_data.Location[200].split(",")[-1].strip()   #.strip to remove any extra whitespaces
 print(country_name)
-countries.get(country_name).alpha3    # alpha three returns 3 letter iso
+print(countries.get(country_name).alpha3)   # alpha three returns 3 letter iso
 
 
-# In[509]:
+# In[235]:
 
 
 #Putting this into a function
@@ -278,7 +270,7 @@ def get_iso(location):
 df_data['ISO'] = df_data['Location'].apply(get_iso)
 
 
-# In[510]:
+# In[236]:
 
 
 # making a new dataframe with the counts
@@ -287,7 +279,7 @@ launches_by_country = df_data.ISO.value_counts().reset_index()
 launches_by_country
 
 
-# In[511]:
+# In[237]:
 
 
 map = px.choropleth(launches_by_country, 
@@ -299,34 +291,10 @@ map = px.choropleth(launches_by_country,
 map.show()
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
 # # Use a Choropleth Map to Show the Number of Failures by Country
 # 
 
-# In[512]:
-
-
-df_data.head()
-
-
-# In[513]:
-
-
-df_data.loc[df_data['Mission_Status'] != 'Success'].tail(30)
-
-
-# In[514]:
+# In[238]:
 
 
 # first we need to group all failure types together: this code handles it by creating a new dataframe to include only failures
@@ -336,14 +304,14 @@ df_data.loc[df_data['Mission_Status'] != 'Success'].tail(30)
 failures = df_data.loc[df_data['Mission_Status'] != 'Success'].groupby(['ISO'], as_index = False).agg({'Mission_Status':pd.Series.count})
 
 
-# In[515]:
+# In[239]:
 
 
 # renaming the columns
 failures.rename(columns={'Mission_Status':'Failures'}, inplace= True)
 
 
-# In[516]:
+# In[240]:
 
 
 map = px.choropleth(failures, 
@@ -355,21 +323,15 @@ map = px.choropleth(failures,
 map.show()
 
 
-# In[ ]:
-
-
-
-
-
 # # Create a Plotly Sunburst Chart of the countries, organisations, and mission status. 
 
-# In[517]:
+# In[241]:
 
 
 df_data
 
 
-# In[518]:
+# In[242]:
 
 
 # Creating a dataframe that groups by country, org, and mission status, then count occurences of each combination.
@@ -377,7 +339,7 @@ iso_status = df_data.groupby(['ISO', 'Organisation', 'Mission_Status']).size().r
 iso_status
 
 
-# In[519]:
+# In[243]:
 
 
 # Define a custom color mapping for different Mission_Status values
@@ -406,14 +368,14 @@ sunburst.show()
 
 # # Analyse the Total Amount of Money Spent by Organisation on Space Missions
 
-# In[520]:
+# In[244]:
 
 
 # we'll use the clean df with the no cost values dropped for a accurate analysis
 # however, we'll need to convert the dtype to an int64 in order to sum up price
 
 
-# In[521]:
+# In[245]:
 
 
 df_data_clean.info()
@@ -421,7 +383,7 @@ df_data_clean.info()
 
 # <h2> The Price must be converted to a int or float</h2>
 
-# In[522]:
+# In[246]:
 
 
 # first we'll remove all the commas then convert to float
@@ -430,20 +392,20 @@ df_data_clean['Price'] = df_data_clean['Price'].str.replace(',','').astype(float
 
 
 
-# In[523]:
+# In[247]:
 
 
 df_data_clean.info()
 
 
-# In[524]:
+# In[248]:
 
 
 money_spent_per_org = df_data_clean.groupby(['Organisation'], as_index = False).agg({'Price': pd.Series.sum})
 money_spent_per_org
 
 
-# In[525]:
+# In[249]:
 
 
 money_bar = px.bar(
@@ -464,14 +426,14 @@ money_bar.update_yaxes(type = 'log')
 
 # # Analyse the Amount of Money Spent by Organisation per Launch
 
-# In[526]:
+# In[250]:
 
 
 # here we will average the prices to determine a per launch value
 org_avg_spend = df_data_clean.groupby(['Organisation'], as_index = False).agg({'Price': pd.Series.mean})
 
 
-# In[527]:
+# In[251]:
 
 
 money_bar = px.bar(
@@ -490,33 +452,33 @@ money_bar.update_layout(yaxis_title = 'Price in $ Millions',
 money_bar.update_yaxes(type = 'log')
 
 
-# In[528]:
+# In[ ]:
 
 
-df_data.loc[13]
+
 
 
 # # Chart the Number of Launches per Year
 
-# In[529]:
+# In[252]:
 
 
 df_data.Date[105]
 
 
-# In[530]:
+# In[253]:
 
 
 pd.to_datetime(df_data.Date[105], format = "%a %b %d, %Y %H:%M %Z")
 
 
-# In[531]:
+# In[254]:
 
 
 df_data.Date[106]
 
 
-# In[532]:
+# In[255]:
 
 
 pd.to_datetime(df_data.Date[106], format = "%a %b %d, %Y")
@@ -524,7 +486,7 @@ pd.to_datetime(df_data.Date[106], format = "%a %b %d, %Y")
 
 # <p>As shown above, we have two different formats shown across the rows, some dates have a time, while some don't. Passing this through the to_datime method will usually throw an error since the formats are not consistent.</p>
 
-# In[533]:
+# In[256]:
 
 
 # making a function to handle different formats. We will convert all times to UTC
@@ -540,7 +502,7 @@ def date_process (x):
         return pd.to_datetime(x, format = only_date_format, utc = True)
 
 
-# In[534]:
+# In[257]:
 
 
 df_data['Date'] = df_data['Date'].apply(date_process)
@@ -548,7 +510,7 @@ df_data['Date'] = df_data['Date'].apply(date_process)
 
 # <h3>Repeating process for clean DF</h3>
 
-# In[535]:
+# In[258]:
 
 
 df_data_clean['Date'] = df_data_clean['Date'].apply(date_process)
@@ -556,13 +518,13 @@ df_data_clean['Date'] = df_data_clean['Date'].apply(date_process)
 
 # <h3>Checking our work</h3>
 
-# In[536]:
+# In[259]:
 
 
 df_data.info()
 
 
-# In[537]:
+# In[260]:
 
 
 df_data_clean.info()
@@ -570,7 +532,7 @@ df_data_clean.info()
 
 # <h3>Adding columns for year and months</h3>
 
-# In[538]:
+# In[261]:
 
 
 # this will be done by adding a column with the year of launch
@@ -585,7 +547,7 @@ df_data.insert(5, 'Year', years)
 df_data.insert(6, 'Month', months)
 
 
-# In[539]:
+# In[262]:
 
 
 df_data.head()
@@ -593,7 +555,7 @@ df_data.head()
 
 # <h2>Finally, we can start plotting our graph.</h2>
 
-# In[540]:
+# In[263]:
 
 
 # let's group the launches by year first
@@ -601,7 +563,7 @@ launches_per_year = df_data.Year.value_counts()
 
 
 
-# In[541]:
+# In[264]:
 
 
 year_launches = px.bar(launches_per_year,
@@ -624,7 +586,7 @@ year_launches.show()
 # 
 # Which month has seen the highest number of launches in all time? Superimpose a rolling average on the month on month time series chart. 
 
-# In[542]:
+# In[265]:
 
 
 launches_ordered = df_data.sort_values(by='Date')
@@ -633,7 +595,7 @@ launch_counts = launches_ordered.groupby(['Year','Month']).size().reset_index(na
 launch_counts
 
 
-# In[543]:
+# In[266]:
 
 
 # Create a new datetime column to represent Year-Month for plotting
@@ -643,15 +605,15 @@ launch_counts
 
 # <h4>Specifying a moving avg</h4>
 
-# In[544]:
+# In[295]:
 
 
-# we'll take 4 observations and average them
-launch_counts['Roll_avg_launches']= launch_counts.Launches.rolling(window = 4).mean()
+# we'll take 6 observations and average them
+launch_counts['Roll_avg_launches']= launch_counts.Launches.rolling(window = 6).mean()
 launch_counts
 
 
-# In[550]:
+# In[296]:
 
 
 year_launches = px.line(launch_counts,
@@ -675,14 +637,14 @@ year_launches.show()
 # 
 # Some months have better weather than others. Which time of year seems to be best for space missions?
 
-# In[552]:
+# In[269]:
 
 
 month_popularity = df_data.Month.value_counts()
 month_popularity
 
 
-# In[555]:
+# In[270]:
 
 
 month_bar = px.bar(month_popularity,
@@ -703,14 +665,14 @@ month_bar.show()
 # 
 # Create a line chart that shows the average price of rocket launches over time. 
 
-# In[560]:
+# In[271]:
 
 
 #First sort the data by date, and use the clean_df which has no cost missions removed.
 sorted_price = df_data_clean.sort_values('Date')
 
 
-# In[574]:
+# In[272]:
 
 
 cost_line = px.scatter(sorted_price,
@@ -743,62 +705,103 @@ cost_line.show()
 # 
 # How has the dominance of launches changed over time between the different players? 
 
-# In[ ]:
+# In[297]:
 
 
+# find top 10 organisation by launch count
+top_10_orgs = df_data['Organisation'].value_counts().nlargest(10).index
+
+# filter dataframe for only top 10 organisations here is pass in the index we made earlier into the isin method   df_data['Organisation'].isin(top_10_orgs) returns a boolean series, where True means the data is in the index defined earlier. 
+# lastly it is encolosed by df_data so only rows marked as true are shown in the dataframe
+filtered_df = df_data[df_data['Organisation'].isin(top_10_orgs)]
 
 
+# group data by date let's aggregate all the launches per year
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
+launches_sorted = filtered_df.groupby([filtered_df['Date'].dt.year, 'Organisation']).size().reset_index(name= 'Launches')
+launches_sorted
 
 
+# In[299]:
 
 
+# plot
+
+fig = px.line(launches_sorted,
+              x = 'Date',
+              y='Launches',
+              color = 'Organisation',
+              title = 'Launches over time by Organisation')
+fig.show()
+
+
+# <p>The USSR led in space launches from 1963 to 1992 (collapse of Soviet Union). Space launches between organisations have been largely equal, until 2016 where the CASC began to take the lead.</p>
 
 # # Cold War Space Race: USA vs USSR
 # 
 # The cold war lasted from the start of the dataset up until 1991. 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
 # ## Create a Plotly Pie Chart comparing the total number of launches of the USSR and the USA
 # 
 # Hint: Remember to include former Soviet Republics like Kazakhstan when analysing the total number of launches. 
 
-# In[ ]:
+# In[300]:
+
+
+# first filter the df to only include years before 1991
+years_sorted = df_data[df_data['Year'] < 1991 ].sort_values(by='Date')
+years_sorted
+
+
+# In[302]:
+
+
+# replace KAZ with RUS to trat KAZ as part of Russia
+years_sorted['ISO'] = years_sorted['ISO'].replace({'KAZ': 'RUS'})
+
+# since Kaz was part of the Soviet union, when sorting by ISO, Kaz will be considered as part of the USSR
+USSR_US = ['USA', 'RUS']
+
+#filter out df by these two countries
+years_sorted = years_sorted.loc[years_sorted['ISO'].isin(USSR_US)]
+
+
+# In[303]:
+
+
+years_sorted = years_sorted.groupby(['Year', 'ISO']).size().reset_index(name= 'Launches')
 
 
 
+# In[304]:
 
 
-# In[ ]:
-
-
-
+us_soviet_pie = px.pie(years_sorted,
+                       values = 'Launches',
+                       names = 'ISO',
+                       title = 'US vs USSR Launches during the Cold War')
+us_soviet_pie.show()
 
 
 # ## Create a Chart that Shows the Total Number of Launches Year-On-Year by the Two Superpowers
 
-# In[ ]:
+# In[305]:
 
 
+# use the prior years sorted dataframe since it is already setup
 
+us_soviet_line = px.line(years_sorted,
+                         x='Year',
+                         y = 'Launches',
+                         color = 'ISO',
+                         title = 'USA and USSR Launches during Cold War')
+
+# set x axis to increment every 5 years
+us_soviet_line.update_xaxes(dtick = 1)
+                        
+us_soviet_line.update_layout(yaxis_title = 'Launches',
+                            legend_title_text = 'Country')
+    
 
 
 # In[ ]:
@@ -809,86 +812,175 @@ cost_line.show()
 
 # ## Chart the Total Number of Mission Failures Year on Year.
 
-# In[ ]:
+# In[306]:
+
+
+# first filter out dataframe to where no successes are shown, this is more straightforward since there are up to 3 types of failures
+failures_df = df_data[df_data['Mission_Status'] != 'Success']
+
+# then by group by year only, as a result all failures get grouped together since this is what we want for the graph
+failures_yoy = failures_df.groupby(['Year'], as_index = False).size()
 
 
 
+# In[307]:
 
 
-# In[ ]:
+failures_line = px.bar(failures_yoy,
+                        x='Year',
+                        y='size',
+                        title = 'Space Launch Failures',
+                      color_continuous_scale = 'brwnyl',
+                      color = 'size')
 
-
-
+failures_line.update_layout(yaxis_title = 'Failures', coloraxis_showscale = False)
 
 
 # ## Chart the Percentage of Failures over Time
 # 
 # Did failures go up or down over time? Did the countries get better at minimising risk and improving their chances of success over time? 
 
-# In[ ]:
+# In[283]:
 
 
+# find total launches
+
+total_launches = df_data.groupby(['Year']).size().reset_index(name= 'Total_Launches')
+
+#filter out to only include failures
 
 
-
-# In[ ]:
-
+# In[308]:
 
 
+# add total launches column
+failures_yoy_pct = failures_yoy
+failures_yoy_pct['total_launches'] = total_launches['Total_Launches']
+
+# calculate failure pct
+failures_yoy_pct['fail_pct'] = (failures_yoy_pct['size']/failures_yoy_pct['total_launches'])*100
 
 
-# In[ ]:
+# In[309]:
 
 
+figure_pct = px.bar(failures_yoy_pct,
+        x='Year',
+        y='fail_pct',
+        title = 'Failure Percentage for Space Shuttle Missions'
+)
+
+figure_pct.show()
+        
 
 
+# <p>From the graph, failure rates have drastically decreased since 1957. After 1975 however, rates have more or less plateaued from 3 to 10 percent.</p>
 
 # # For Every Year Show which Country was in the Lead in terms of Total Number of Launches up to and including including 2020)
 # 
 # Do the results change if we only look at the number of successful launches? 
 
-# In[ ]:
+# In[310]:
+
+
+# group by country
+iso_yearly = df_data.groupby(['Year', 'ISO']).size().reset_index(name = 'Launches')
+
+#replace KAZ with RUS since KAZ was part of the Soviet Union
+iso_yearly['ISO'] = iso_yearly['ISO'].replace({'KAZ':'RUS'})
 
 
 
+# In[311]:
 
 
-# In[ ]:
+# sum number of launches - first group df by year, for each year access the Launches column to get total number of launches corresponding to each country. idxmax to return index of row.
+#  .loc to return identity
+lead_org_df = iso_yearly.loc[iso_yearly.groupby('Year')['Launches'].idxmax()]
+
+
+# In[312]:
+
+
+lead_org_bar = px.bar(lead_org_df,
+                      x='Launches',
+                      y='Year',
+                      color = 'ISO',
+                      title = 'Leading country in Space Launches YOY',
+                     orientation = 'h',
+                     width=1200, height=1000)
+lead_org_bar.show()
+
+
+# <p>From the chart, the USA was dominant for the first 7 years of the space race, with the soviet union taking the lead from 1964 to it's Collapse in 1991. The US led from there on until 2018, where China began to overtake the US.</p>
+
+# <h3>Now only considering successful launches,</h3>
+
+# In[313]:
+
+
+# let's filter df only to include successful launches
+success_df = df_data[df_data['Mission_Status'] == 'Success'].sort_values(by='Year')
+
+# group by country
+country_success = success_df.groupby(['Year', 'ISO']).size().reset_index(name = 'Launches')
+
+# map KAZ as Russia
+country_success['ISO'] = country_success['ISO'].replace({'KAZ':'RUS'})
 
 
 
+# In[314]:
 
+
+country_success = country_success.loc[country_success.groupby('Year')['Launches'].idxmax()]
+
+
+# In[315]:
+
+
+success_lead = px.bar(country_success,
+                      x='Launches',
+                      y='Year',
+                      color = 'ISO',
+                      title = 'Leading country in Successful Space Launches YOY',
+                     orientation = 'h',
+                     width=1200, height=1000)
+success_lead.show()
+
+
+# <p>The trend is nearly similar, however China does lose out to the US in 2020 once failures are accounted for. In 1993 Russia beats the US once failures are accounted for.</p>
 
 # # Create a Year-on-Year Chart Showing the Organisation Doing the Most Number of Launches
 # 
 # Which organisation was dominant in the 1970s and 1980s? Which organisation was dominant in 2018, 2019 and 2020? 
 
-# In[ ]:
+# In[317]:
 
 
+# here we'll use the launches_sorted df since it already is neatly sorted
 
 
-
-# In[ ]:
-
+# In[318]:
 
 
+# now we find winner per year
+winning_org = launches_sorted.loc[launches_sorted.groupby('Date')['Launches'].idxmax()]
+winning_org.tail(20)
 
 
-# In[ ]:
+# In[319]:
 
 
+leading_org = px.bar(winning_org,
+                      x='Launches',
+                      y='Date',
+                      color = 'Organisation',
+                      title = 'Leading Organisation in Space Launches YOY',
+                     orientation = 'h',
+                     width=1200, height=1000)
+leading_org.update_layout(yaxis_title = 'Year')
+leading_org.show()
 
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+# <p>The USSR was the dominant organisation in the 70's and 80's. From 2016 to 2020, the CASC lead in launches.</p>
